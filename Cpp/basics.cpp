b8969e89c8831bc07199b4453a41abf033cda4c5
Saurabh-rana-01 <ranasaurabh072@gmail.com>
2026-08-04 23:02:58 +0530

    If Loop

--- Cpp/basics.cpp
+++ Cpp/basics.cpp
@@ -6,6 +6,12 @@ int main() {
    cout << " Enter the Values of Two Numbers:" << endl;
    cin >> num1 >> num2;
    cout << " Num1:"<<num1 << "\n Num2:" << num2 << endl;
+   if(num1>num2){
+    cout<<"The sum of num1 and num2 is : "<< num1+num2 << endl;
+
+   }else{
+    cout<<"The difference of num1 and num2 is : "<< num1-num2 << endl;
+   }
 
     return 0;
 }
