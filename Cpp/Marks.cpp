a5f7d30431cfa0ad06ea3ba76b1971df54d35ce0
Saurabh-rana-01 <ranasaurabh072@gmail.com>
2026-08-08 03:37:05 +0530

    If Statement

--- Cpp/Marks.cpp
+++ Cpp/Marks.cpp
@@ -4,26 +4,24 @@ int main(){
     int marks;
     cout<<"Enter the marks of the students: "<<endl;
     cin>>marks;
-    switch(marks){
-        case 100:
-            cout<<"The student has scored full marks."<<endl;
-            break;
-        case 90:
-            cout<<"The student has scored very good marks."<<endl;
-            break;
-        case 80:
-            cout<<"The student has scored good marks."<<endl;
-            break;
-        case 70:
-            cout<<"The student has scored average marks."<<endl;
-            break;
-        default:
-            cout<<"The student has scored below average marks."<<endl;
-    }
-
-
-
-
+   if(marks< 25){
+   cout<<"GradeF"<<endl;
+   }
+   if(marks>=25 && marks<=44){
+    cout<<"Grade E"<<endl;
+   }
+   if(marks>=45&& marks<=49){
+    cout<<"Grade D"<<endl;
+   }
+   if(marks>=50 && marks<=59){
+    cout<<"Grade C"<<endl;
+   }
+   if(marks>=60 && marks<=69){
+    cout<<"Grade B"<<endl;
+   }
+   if(marks>=70 && marks<=100){
+    cout<<"Grade A"<<endl;
+   }
 
 
     return 0;
