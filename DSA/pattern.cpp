1cfc5ffbf62c8551e614fec40c338e9ef760619d
Saurabh-rana-01 <ranasaurabh072@gmail.com>
2026-08-08 18:19:18 +0530

    Pattern Print

---
+++ DSA/pattern.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+void print(){
+    for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+
+    }
+}
+
+ int main(){
+    print();
+    return 0;
+  }
