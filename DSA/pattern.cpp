3b4b3981d57766d38e31bd807c9e9e9811dba6c9
Saurabh-rana-01 <ranasaurabh072@gmail.com>
2026-08-08 18:40:41 +0530

    Add Patttern2

--- DSA/pattern.cpp
+++ DSA/pattern.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 void print(int n){
     for(int i=0;i<n;i++){
-        for(int j=0;j<4;j++){
+        for(int j=0;j<=i;j++){
             cout<<"*";
         }
         cout<<endl;
