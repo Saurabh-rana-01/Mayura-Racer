f175b8f2dc9d875a3ddce1058cf74fc2bd6a6e05
Saurabh-rana-01 <ranasaurabh072@gmail.com>
2026-08-10 07:22:09 +0530

    Add all in one

---
+++ DSA/Patterns.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+using namespace std;
+
+void print(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=n;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+void print2(int n){
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+void print3(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
+
+void print4(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<i;
+        }
+        cout<<endl;
+    }
+}
+
+void print5(int n){
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=n-i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+void print6(int n){
+
+    for(int i=0;i<=n;i++){
+        for (int j = 0; j <= n - i; j++) {
+            cout << " ";
+        }
+        for(int j=0;j<1+i*2;j++){
+            cout<<"*";
+        }
+    
+        cout<<endl;
+    }
+}
+
+
+
+
+
+int main(){
+    int n;
+    cin>>n;
+
+    print6(n);
+    return 0;
+}
--- DSA/pattern4.cpp
+++ DSA/pattern4.cpp
@@ -3,11 +3,13 @@ using namespace std;
 
 void print(int n){
     for(int i=1;i<n;i++){
-        for(int j=1;j<=i;j++)
-           cout<< j;
-           cout<<endl;
+        for(int j=1;j<=i;j++){
+           cout<< i << " ";
+         
        }
+         cout<<endl;
        
+}
 }
 
 int main(){
