8c4c98acdead95f39ffc4685a9276b9373faab20
Saurabh-rana-01 <ranasaurabh072@gmail.com>
2026-08-16 23:47:19 +0530

    Vector All Topics

--- STL CPP/stl1.cpp
+++ STL CPP/stl1.cpp
@@ -1,38 +1,72 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm> // for sort, reverse, find
 using namespace std;
 
-void print(){
-    cout<<"Hello World";
-}
+int main() {
+    // 1. Initialization
+    vector<int> v1;                     // empty vector
+    vector<int> v2(5, 10);              // 5 elements, all 10
+    vector<int> v3 = {1, 2, 3, 4, 5};   // initializer list
 
-int sum(int a, int b){
-    return a+b;
-}
+    cout << "Initial v3: ";
+    for(int x : v3) cout << x << " ";
+    cout << "\n";
 
-void explainPair(){
-   pair<int, int> p={1,3};
-   cout<<p.first<<" "<<p.second<<endl;
-   pair<int, pair<int, int>>p1={1,{3,4}};
-   cout<<p1.first<<" "<<p1.second.first<<" "<<p1.second.second<<endl;
-   pair<int, int> arr[] ={{1,2},{2,5},{5,1}
-   };
-   for(int i=0;i<3;i++){
-       cout<<arr[i].first<<" "<<arr[i].second<<endl;
-   }
-}
+    // 2. Adding elements
+    v3.push_back(6);   // add at end
+    v3.push_back(7);
 
+    // 3. Removing elements
+    v3.pop_back();     // removes last element
+    v3.erase(v3.begin() + 1); // remove second element
 
-int main(){
-    print();
-    cout<<endl;
-    cout<<sum(5, 10)<<endl;
-    explainPair();
+    // 4. Insert element
+    v3.insert(v3.begin() + 2, 99);
 
+    // 5. Access elements
+    cout << "First element: " << v3.front() << "\n";
+    cout << "Last element: " << v3.back() << "\n";
+    cout << "Element at index 2: " << v3.at(2) << "\n";
 
+    // 6. Iterators
+    cout << "Using iterators: ";
+    for(auto it = v3.begin(); it != v3.end(); ++it)
+        cout << *it << " ";
+    cout << "\n";
 
+    cout << "Reverse iterators: ";
+    for(auto rit = v3.rbegin(); rit != v3.rend(); ++rit)
+        cout << *rit << " ";
+    cout << "\n";
 
+    // 7. Capacity functions
+    cout << "Size: " << v3.size() << "\n";
+    cout << "Capacity: " << v3.capacity() << "\n";
+    v3.resize(10, -1);   // resize to 10, fill with -1
+    cout << "After resize: ";
+    for(int x : v3) cout << x << " ";
+    cout << "\n";
+    v3.reserve(20);      // reserve capacity
+    cout << "Capacity after reserve: " << v3.capacity() << "\n";
+    cout << "Is empty? " << (v3.empty() ? "Yes" : "No") << "\n";
 
+    // 8. Algorithms
+    sort(v3.begin(), v3.end());   // sort ascending
+    cout << "Sorted: ";
+    for(int x : v3) cout << x << " ";
+    cout << "\n";
+
+    reverse(v3.begin(), v3.end()); // reverse order
+    cout << "Reversed: ";
+    for(int x : v3) cout << x << " ";
+    cout << "\n";
+
+    auto it = find(v3.begin(), v3.end(), 99);
+    if(it != v3.end())
+        cout << "Found 99 at position: " << (it - v3.begin()) << "\n";
+    else
+        cout << "99 not found\n";
 
     return 0;
-
 }
